85a9f7ac253c05fd83245386c0bc29d3719137bf
Zahra Saffarpour <Saffarpour.Zahra@okco.ir>
2023-10-05 11:48:57 +0330

    get folder information and security by folder path

---
+++ Functions/Get-API-Folder-Item.ps1
@@ -0,0 +1,65 @@
+﻿function Get-RsFolderItem {
+    <#
+        .SYNOPSIS
+            This function gets an Json of Folder CatalogItems.
+
+        .DESCRIPTION
+            This function gets an Json of Folder CatalogItems.
+
+        .PARAMETER WebPortalURL
+            #Specify the name of the WebPortalURL.
+
+        .PARAMETER Credential
+            Specify the credentials to use when connecting to the Report Server.
+
+        .PARAMETER FolderPath
+            Specify the Folder Path.
+
+        .PARAMETER ErrorFile
+            Specify the path to save the exceptions in the file.
+
+        .EXAMPLE
+            $myCredential = Get-Credential
+            Get-RsFolderItem -WebPortalURL "http://localhost/reports" -Credential $myCredential -FolderPath "/Dataset" -ErrorFile "C:\Temp\Error_20231003.txt"
+            Description
+            -----------
+            
+        .LINK
+            https://app.swaggerhub.com/apis/microsoft-rs/PBIRS/2.0#/Folders/GetFolders
+    #>    
+    [CmdletBinding()]
+    param 
+    (
+        [Parameter(Mandatory = $True, ValueFromPipelineByPropertyName = $true)]
+        $WebPortalURL,
+        [System.Management.Automation.PSCredential] 
+        $Credential,
+        [Parameter(Mandatory = $True, ValueFromPipelineByPropertyName = $true)]
+        $FolderPath,
+        $ErrorFile
+    )
+    Begin {
+        $myFolderAPI = $WebPortalURL + "/api/v2.0/Folders(path='" + $FolderPath + "')"
+        $mySpliter = ("--" + ("==" * 70))
+    }
+    Process {
+        try {
+            if ($null -ne $Credential) {
+                $myResponse = Invoke-RestMethod -Method Get -Uri $myFolderAPI -Credential $Credential -ContentType 'application/json; charset=unicode' -Verbose:$false
+            }
+            else {
+                $myResponse = Invoke-RestMethod -Method Get -Uri $myFolderAPI -ContentType 'application/json; charset=unicode' -UseDefaultCredentials -Verbose:$false
+            }
+            $myResultJSON = $myResponse | Select-Object Id, Name, Description, Path, Type, Hidden, CreatedBy, CreatedDate, ModifiedBy, ModifiedDate| ConvertTo-Json -Depth 15   
+            return , $myResultJSON 
+        }
+        catch {
+            if ($null -ne $ErrorFile -and $ErrorFile.Length -gt 0) {
+                "Function : Get-RsFolderItem" >> $ErrorFile
+                "Folder Path : $FolderPath"  >> $ErrorFile
+                $_ >> $ErrorFile  
+                $mySpliter >> $ErrorFile 
+            }
+        }
+    }
+}
---
+++ Functions/Get-API-Folders-Policy-Item.ps1
@@ -0,0 +1,86 @@
+﻿
+function Get-RsFolderPolicyItem {
+    <#
+        .SYNOPSIS
+            This function gets policies associated with the Folder CatalogItem.
+
+        .DESCRIPTION
+            This function gets policies associated with the Folder CatalogItem.
+
+        .PARAMETER WebPortalURL
+            #Specify the name of the WebPortalURL.
+
+        .PARAMETER Credential
+            Specify the credentials to use when connecting to the Report Server.
+
+        .PARAMETER FolderPath
+            Specify the Folder Path.
+            
+        .PARAMETER ErrorFile
+            Specify the path to save the exceptions in the file.
+
+        .EXAMPLE
+            $myCredential = Get-Credential
+            Get-RsFolderPolicyItem -WebPortalURL "http://localhost/reports" -Credential $myCredential -FolderPath "/Dataset" -ErrorFile "C:\Temp\Error_20231003.txt"
+            Description
+            -----------
+            
+        .LINK
+            https://app.swaggerhub.com/apis/microsoft-rs/PBIRS/2.0#/Folders/GetFolderPolicies
+    #>  
+    [CmdletBinding()]
+    param 
+    (
+        [Parameter(Mandatory = $True, ValueFromPipelineByPropertyName = $true)]
+        $WebPortalURL,
+        [System.Management.Automation.PSCredential] 
+        $Credential,
+        [Parameter(Mandatory = $True, ValueFromPipelineByPropertyName = $true)]
+        $FolderPath,
+        $ErrorFile
+    )
+    Begin {
+        $myFolderAPI = $WebPortalURL + "/api/v2.0/Folders(path='" + $FolderPath + "')"
+        $myFolderPolicyAPI = $myFolderAPI + "/Policies"
+        $myFolderPolicyItems = New-Object System.Collections.ArrayList
+        $mySpliter = ("--" + ("==" * 70))
+    }
+    Process {
+        try {
+            if ($null -ne $Credential) {
+                $myResponse = Invoke-RestMethod -Method Get -Uri $myFolderAPI  -Credential $Credential -Verbose:$false
+            }
+            else {
+                $myResponse = Invoke-RestMethod -Method Get -Uri $myFolderAPI -UseDefaultCredentials -Verbose:$false
+            }
+            $myFolderId = $myResponse.Id
+            $myFolderName = $myResponse.Name
+            ##================================================
+            if ($null -ne $Credential) {
+                $myResponse = Invoke-RestMethod -Method Get -Uri $myFolderPolicyAPI  -Credential $Credential -Verbose:$false
+            }
+            else {
+                $myResponse = Invoke-RestMethod -Method Get -Uri $myFolderPolicyAPI -UseDefaultCredentials -Verbose:$false
+            }
+            $myPolicyItems = New-Object System.Collections.ArrayList
+            foreach ($myPolicy in $myResponse.Policies | Sort-Object GroupUserName) {
+                $myRoleItems = New-Object System.Collections.ArrayList
+                foreach ($myRole in $myPolicy.Roles) {
+                    $myRoleItems.Add([PSCustomObject]@{"Name" = $myRole.Name; "Description" = $myRole.Description }) | Out-Null
+                }       
+                $myPolicyItems.Add([PSCustomObject]@{"GroupUserName" = $myPolicy.GroupUserName; "Roles" = $myRoleItems; }) | Out-Null
+            }
+            $myFolderPolicyItems.Add([PSCustomObject]@{"Id" = $myFolderId; "Name" = $myFolderName; "Path" = $FolderPath; "InheritParentPolicy" = $myResponse.InheritParentPolicy; "Policies" = $myPolicyItems }) | Out-Null
+            $myResultJSON = $myFolderPolicyItems | ConvertTo-Json -Depth 15 
+            return , $myResultJSON    
+        }
+        catch {
+            if ($null -ne $ErrorFile -and $ErrorFile.Length -gt 0) {
+                "Function : Get-RsFolderPolicyItem" >> $ErrorFile
+                "Folder Path : $FolderPath"  >> $ErrorFile
+                $_ >> $ErrorFile  
+                $mySpliter >> $ErrorFile 
+            }
+        }
+    }
+}
